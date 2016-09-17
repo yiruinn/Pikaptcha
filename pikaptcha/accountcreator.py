b21726326079d81537491facfbd0ceb6108d13c3
yiruinn <yiruinn@gmail.com>
2016-09-16 21:55:58 -0500

    Update accountcreator.py

--- pikaptcha/accountcreator.py
+++ pikaptcha/accountcreator.py
@@ -123,7 +123,7 @@ def create_account(username, password, email, birthday, captchakey2, captchatime
     
     driver.find_element_by_id("check-availability-username").click()
     time.sleep(2)
-    text = driver.find_element_by_class_name("alert alert-box alert-success")
+    text = driver.find_element_by_class_name("alert")
     print(text)
     
     time.sleep(10)
