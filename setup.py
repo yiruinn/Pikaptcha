0dfce5ef743fcbf793ca3a2e680fd27ebbf2afcd
yiruinn <yiruinn@gmail.com>
2016-11-15 00:37:56 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'requests[security]==2.10.0',
         'six==1.10.0',
-        'selenium==2.53.6'
+        'selenium==3.0.1'
     ],
     classifiers=[
         'Programming Language :: Python',
