6dfdb67000c4da400a1e3dcd45257857af6f94e9
yiruinn <yiruinn@gmail.com>
2017-04-13 22:32:28 -0500

    Shit's fixed yo. Proxy support + limit check.

--- pikaptcha/accountcreator.py
+++ pikaptcha/accountcreator.py
@@ -176,7 +176,7 @@ def create_account(username, password, email, birthday, captchakey2, captchatime
 
     elem = driver.find_element_by_name("email")
     elem.clear()
-    elem.send_keys(email)("button-green")
+    elem.send_keys(email)
     elem.submit()
     
 
