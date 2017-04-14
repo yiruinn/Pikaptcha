3beb52760ef39ef22d7ef905454271b88580ecca
yiruinn <yiruinn@gmail.com>
2017-04-13 19:01:53 -0500

    Update accountcreator.py

--- pikaptcha/accountcreator.py
+++ pikaptcha/accountcreator.py
@@ -99,11 +99,13 @@ def create_account(username, password, email, birthday, captchakey2, captchatime
             dcap["phantomjs.page.settings.userAgent"] = user_agent
             driver = webdriver.PhantomJS(desired_capabilities=dcap,service_args=service_args)
             print("Using proxy for phantomjs: " + proxy)
+            driver.set_window_size(1200, 800)
         else:
             dcap = dict(DesiredCapabilities.PHANTOMJS)
             dcap["phantomjs.page.settings.userAgent"] = user_agent
             driver = webdriver.PhantomJS(desired_capabilities=dcap)
             print("Not using proxy for phantomjs")
+            driver.set_window_size(1200, 800)
     else:
         if proxy is not None:
             chrome_options = webdriver.ChromeOptions()
