2ab58c93913fb8cf69a7a35835263d54d0aab657
yiruinn <yiruinn@gmail.com>
2017-04-15 14:06:50 -0500

    Update accountcreator.py

--- pikaptcha/accountcreator.py
+++ pikaptcha/accountcreator.py
@@ -230,8 +230,6 @@ def create_account(username, password, email, birthday, captchakey2, captchatime
                     _validate_response(driver)
                 except:
                     print("2Captcha failed to solve. Unable to create user.")
-                    if raw_input("Continue? y/n") == "y":
-                        print("Continuing")
                     driver.close()
                     raise
         if timedout == False:       
